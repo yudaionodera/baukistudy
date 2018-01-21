aad9cca6382e63efbe14c7e444fd7b102b4f52be
yudaionodera <yuudai.2451@gmail.com>
2018-01-21 16:45:48 +0900

    タイムゾーンとロケールの設定

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,10 @@ Bundler.require(*Rails.groups)
 module Baukistudy
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
+    config.time_zone = 'TOKYO'
+    config.i18n.load_path +=
+      Dir[Rails.root,join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :ja
     config.load_defaults 5.1
 
     # Settings in config/environments/* take precedence over those specified here.
